5bd77a68da776ec0db40acf92091a26f20010942
Sawyerbian <84435066+Sawyerbian@users.noreply.github.com>
2022-11-25 23:38:37 +0800

    update token

--- influxdb_config.py
+++ influxdb_config.py
@@ -3,7 +3,8 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from tqdm import tqdm
 def connection():
-    token = os.environ.get("INFLUXDB_TOKEN")
+    #token = os.environ.get("INFLUXDB_TOKEN")
+    token = 'v-k-YeEWpI0-UJVI666LLMPLTLqDvgpDBnlKO_0w9DW0uEQlNMdngegO_kV_TtFCwtH9uYKlTYw1EBj0EWCl1Q=='
     org = "battery"
     #url = "https://us-west-2-1.aws.cloud2.influxdata.com/"
 
